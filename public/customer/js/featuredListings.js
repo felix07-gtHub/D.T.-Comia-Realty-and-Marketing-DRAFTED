8618d4321b055727f17414861b9a28cfe604adb8
felix07-gtHub <olan.johnfelix@gmail.com>
2025-10-14 15:20:48 +0800

    made some changes

--- public/customer/js/featuredListings.js
+++ public/customer/js/featuredListings.js
@@ -6,6 +6,8 @@ async function featuredProperty() {
     });
     const data = await response.json();
 
+    consolo.log(data);
+
     const listings = document.querySelector('#featuredListings');
 
         //  CLEAR THE #listings BEFORE DISPLAYING THE UPDATED PROPERTY LISTINGS.
@@ -168,7 +170,6 @@ async function featuredProperty() {
 
             property.classList.add('property');
             imageContainer.classList.add('imageContainer');
-            console.log(data.featuredPropertyListings[i])
             propertyImage.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].location.replace(/[,]/g, "").replace(/[ ]/g, "_") + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].main_image;
             propertyImage.alt =  data.featuredPropertyListings[i].main_image;
             propertyImage.type = "";
