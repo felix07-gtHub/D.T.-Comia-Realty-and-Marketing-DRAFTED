cfd68c2afbc3cedc7968aa65aa44c7dd4f080807
felix07-gtHub <olan.johnfelix@gmail.com>
2025-10-14 14:54:30 +0800

    made some changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://artclassic-production.up.railway.app', // Allow only a specific origin
+  origin: 'https://dt-comia-realty-and-marketing-production.up.railway.app', // Allow only a specific origin
   credentials: true,            // Enable cookies and credentials
 }));
 app.use(session({
