0aec5f26a2be289cd909e9195e64141ca50ecfce
felix07-gtHub <olan.johnfelix@gmail.com>
2025-10-16 23:51:50 +0800

    made some changes

--- public/customer/js/featuredListings.js
+++ public/customer/js/featuredListings.js
@@ -168,16 +168,16 @@ async function featuredProperty() {
 
             property.classList.add('property');
             imageContainer.classList.add('imageContainer');
-            propertyImage.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].main_image;
+            propertyImage.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].main_image;
             propertyImage.alt =  data.featuredPropertyListings[i].main_image;
             propertyImage.type = "";
             price.innerHTML = "&#8369;" + data.featuredPropertyListings[i].price_formatted;
             address.innerHTML = data.featuredPropertyListings[i].address;
-            bedIcon.src = "../resources/BUYER ICONS AND LOGOS/green bed.png";
+            bedIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/green bed.png";
             bedIcon.alt = "Bed icon";
             bedIcon.type = "";
             bedCount.innerHTML = data.featuredPropertyListings[i].room_count;
-            bathtubIcon.src = "../resources/BUYER ICONS AND LOGOS/black bathtub.png";
+            bathtubIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/black bathtub.png";
             bathtubIcon.alt = "Bathtub icon";
             bathtubIcon.type = "";
             bathtubCount.innerHTML = data.featuredPropertyListings[i].bath_count;
@@ -233,12 +233,12 @@ async function featuredProperty() {
                 if(data.savedPropertyListings.length > 0) {
                     for(let j = 0; j < data.savedPropertyListings.length; j++) {
                         if(data.featuredPropertyListings[i].property_id != data.savedPropertyListings[j].property_id) {
-                            savedIcon.src = "../resources/BUYER ICONS AND LOGOS/emptyheart.png";
+                            savedIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/emptyheart.png";
                             savedIcon.alt = "Heart icon empty";
                             savedIcon.type = "";
                         } else {
                             savedInput.checked = true;
-                            savedIcon.src = "../resources/BUYER ICONS AND LOGOS/whole heart.png";
+                            savedIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/whole heart.png";
                             savedIcon.alt = "Heart icon filled";
                             savedIcon.type = "";
 
@@ -246,7 +246,7 @@ async function featuredProperty() {
                         };
                     };
                 } else {
-                    savedIcon.src = "../resources/BUYER ICONS AND LOGOS/emptyheart.png";
+                    savedIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/emptyheart.png";
                     savedIcon.alt = "Heart icon empty";
                     savedIcon.type = "";
                 };
@@ -265,9 +265,9 @@ async function featuredProperty() {
                     });
 
                     if(savedInput.checked != true) {
-                        savedIcon.src = "../resources/BUYER ICONS AND LOGOS/emptyheart.png";
+                        savedIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/emptyheart.png";
                     } else {
-                        savedIcon.src = "../resources/BUYER ICONS AND LOGOS/whole heart.png";
+                        savedIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/whole heart.png";
                     };
 
                 };
@@ -288,7 +288,7 @@ async function featuredProperty() {
                 savedInput.disabled = true;
                 savedLabel.htmlFor = data.featuredPropertyListings[i].property_id;
                 savedLink.href = "./signUp.html";
-                savedIcon.src = "../resources/BUYER ICONS AND LOGOS/emptyheart.png";
+                savedIcon.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/emptyheart.png";
                 savedIcon.alt = "Heart icon empty";
                 savedIcon.type = "";
                 savedIcon.classList.add('savedIcon');
@@ -459,7 +459,7 @@ async function featuredProperty() {
 
             details.classList.add('details');
             imageContainerModal.classList.add('imageContainer');
-            propertyMainImage.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].main_image;
+            propertyMainImage.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].main_image;
             propertyMainImage.alt = data.featuredPropertyListings[i].main_image;
             propertyMainImage.type = "";
             
@@ -622,7 +622,7 @@ async function featuredProperty() {
 
                 const image1 = document.createElement('img');
 
-                image1.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_1;
+                image1.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_1;
                 image1.alt =  data.featuredPropertyListings[i].image_1;
                 image1.type = "";
                 
@@ -634,7 +634,7 @@ async function featuredProperty() {
 
                 const image2 = document.createElement('img');
 
-                image2.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_2;
+                image2.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_2;
                 image2.alt =  data.featuredPropertyListings[i].image_2;
                 image2.type = "";
                 
@@ -646,7 +646,7 @@ async function featuredProperty() {
 
                 const image3 = document.createElement('img');
 
-                image3.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_3;
+                image3.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_3;
                 image3.alt =  data.featuredPropertyListings[i].image_3;
                 image3.type = "";
                 
@@ -658,7 +658,7 @@ async function featuredProperty() {
 
                 const image4 = document.createElement('img');
 
-                image4.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_4;
+                image4.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_4;
                 image4.alt =  data.featuredPropertyListings[i].image_4;
                 image4.type = "";
                 
@@ -670,7 +670,7 @@ async function featuredProperty() {
 
                 const image5 = document.createElement('img');
 
-                image5.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_5;
+                image5.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_5;
                 image5.alt =  data.featuredPropertyListings[i].image_5;
                 image5.type = "";
                 
@@ -682,7 +682,7 @@ async function featuredProperty() {
 
                 const image6 = document.createElement('img');
 
-                image6.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_6;
+                image6.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_6;
                 image6.alt =  data.featuredPropertyListings[i].image_6;
                 image6.type = "";
                 
@@ -694,7 +694,7 @@ async function featuredProperty() {
 
                 const image7 = document.createElement('img');
 
-                image7.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_7;
+                image7.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_7;
                 image7.alt =  data.featuredPropertyListings[i].image_7;
                 image7.type = "";
                 
@@ -706,7 +706,7 @@ async function featuredProperty() {
 
                 const image8 = document.createElement('img');
 
-                image8.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_8;
+                image8.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_8;
                 image8.alt =  data.featuredPropertyListings[i].image_8;
                 image8.type = "";
                 
@@ -718,7 +718,7 @@ async function featuredProperty() {
 
                 const image9 = document.createElement('img');
 
-                image9.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_9;
+                image9.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_9;
                 image9.alt =  data.featuredPropertyListings[i].image_9;
                 image9.type = "";
                 
@@ -730,7 +730,7 @@ async function featuredProperty() {
 
                 const image10 = document.createElement('img');
 
-                image10.src = '../resources/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_10;
+                image10.src = 'https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/PROPERTY/' + data.featuredPropertyListings[i].user_id + '/' + data.featuredPropertyListings[i].property_id + '/' + data.featuredPropertyListings[i].image_10;
                 image10.alt =  data.featuredPropertyListings[i].image_1;
                 image10.type = "";
                 
@@ -749,11 +749,11 @@ async function featuredProperty() {
                 const next = document.createElement('img');
 
                 previousButton.classList.add('previousIcon');
-                previous.src = "../resources/BUYER ICONS AND LOGOS/left arrow-white.png"
+                previous.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/left arrow-white.png"
                 previous.alt = "Previous icon";
                 previous.type = "";
                 nextButton.classList.add('nextIcon');
-                next.src = "../resources/BUYER ICONS AND LOGOS/right arrow.png";
+                next.src = "https://niwxujzmwpdhegjlmyfw.supabase.co/storage/v1/object/public/D.T.%20Comia%20Realty%20and%20Marketing/BUYER ICONS AND LOGOS/right arrow.png";
                 next.alt = "Next icon";
                 next.type = "";
 
