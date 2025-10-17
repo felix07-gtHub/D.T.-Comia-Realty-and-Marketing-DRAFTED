924744f753528ee99f21bbf401e044ebe0e6169d
felix07-gtHub <olan.johnfelix@gmail.com>
2025-10-17 10:46:25 +0800

    Update featuredLandListings.js

--- public/agent/js/featuredLandListings.js
+++ public/agent/js/featuredLandListings.js
@@ -279,7 +279,6 @@ async function featuredLandListings() {
 
         const propertyType = document.createElement('p');
         const hr = document.createElement('hr');
-        const locationModal = document.createElement('a');
         const bedRooms = document.createElement('p');
         const bedRoomsCount = document.createElement('p');
         const bathRooms = document.createElement('p');
@@ -290,8 +289,6 @@ async function featuredLandListings() {
         const div = document.createElement('div');
          
         propertyType.innerHTML = data.featuredLandListings[i].property_type;
-        locationModal.href = "";
-        locationModal.innerHTML = "View Location";
         bedRooms.innerHTML = "Bedrooms";
         bedRoomsCount.innerHTML = data.featuredLandListings[i].room_count;
         bathRooms.innerHTML = "Bathrooms";
@@ -303,7 +300,6 @@ async function featuredLandListings() {
 
         details.appendChild(propertyType);
         details.appendChild(hr);
-        details.appendChild(locationModal);
         details.appendChild(bedRooms);
         details.appendChild(bedRoomsCount);
         details.appendChild(bathRooms);
@@ -578,3 +574,4 @@ async function featuredLandListings() {
 
 
 featuredLandListings().catch(console.error);
+
